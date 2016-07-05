9321e2d814d359465c924b59f626e8352c0bbdaa
Zara Leventhal <zleventhal@gmail.com>
2016-07-05 14:31:35 -0400

    Week 03 changes

--- js/game.js
+++ js/game.js
@@ -37,6 +37,9 @@ var mouseObj = {};
 gameCanvas.width = W;
 gameCanvas.height = H;
 
+
+// Step 02 .. jwt .. Clear page canvas by covering it in black.
+
 function paintCanvas() {
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, W, H);
@@ -46,12 +49,65 @@ paintCanvas();
 
 function trackPosition(evt) {
     mouseObj.x = evt.pageX;
-		mouseObj.y = evt.pageY;
-		// console.log("cursor x is : " + mouseObj.x + "cursor y is : " + mouseObj.y);
+    mouseObj.y = evt.pageY;
+    // console.log("cursor x is : " + mouseObj.x + "cursor y is : " + mouseObj.y);
 }
 
 gameCanvas.addEventListener("mousemove", trackPosition, true);
 
 
+// Step 03 .. jwt .. Place a ball on the canvas
+var ball = {}; // Ball Object
+ball = {
+    x: 50,
+    y: 50,
+    r: 5,
+    c: "#fff",
+    vx: 4,
+    vy: 8,
+    draw: function() {
+        ctx.beginPath();
+        ctx.fillStyle = this.c;
+        ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2, false);
+        ctx.fill();
+    }
+}
 
-//
+ball.draw();
+
+
+// Step 04 .. jwt .. Place a start button on canvas
+var startBtn = {}; // Start button object
+startBtn = {
+    w: 100,
+    h: 50,
+    x: W / 2 - 50,
+    y: H / 2 - 25,
+
+    draw: function() {
+        ctx.strokeStyle = "#fff";
+        ctx.lineWidth = "2";
+        ctx.strokeRect(this.x, this.y, this.w, this.h);
+
+        ctx.font = "18px Arial, sans-serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillStyle = "#fff";
+        ctx.fillText("START", W / 2, H / 2);
+    }
+}
+
+startBtn.draw();
+
+// Step 05 .. jwt .. Place score and points on canvas
+var points = 0; // game points
+function paintScore() {
+    ctx.fillStyle = "#fff";
+    ctx.font = "18px Arial, sans-serif";
+    ctx.textAlign = "left";
+    ctx.textBaseline = "top";
+    ctx.fillText("SCORE: " + points, 20, 20);
+
+}
+
+paintScore();
